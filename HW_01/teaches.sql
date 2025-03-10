88f03154dfc8b1e2dcc028c7211226a88bbc8359
Sayhello-Bro <KKDaniel6821@gmail.com>
2025-03-10 20:46:55 +0800

    add teaches.sql

---
+++ HW_01/teaches.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- 主機: localhost
+-- 產生時間： 2025-03-10 12:44:14
+-- 伺服器版本: 5.7.17-log
+-- PHP 版本： 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `20250304`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `teaches`
+--
+
+CREATE TABLE `teaches` (
+  `ID` varchar(5) DEFAULT NULL,
+  `course_id` varchar(8) DEFAULT NULL,
+  `sec_id` varchar(8) DEFAULT NULL,
+  `semester` varchar(6) DEFAULT NULL,
+  `year` decimal(4,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
