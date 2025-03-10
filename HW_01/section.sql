a5bd72da2b880e68d13480eedd08a78c7ee80d0b
redfish <chenredfish940309@gmsil.com>
2025-03-10 16:49:49 +0800

    魟瑜的第一份作業上傳

---
+++ HW_01/section.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-03-10 09:48:42
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `20250304`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `section`
+--
+
+CREATE TABLE `section` (
+  `course_id` varchar(8) NOT NULL,
+  `sec_id` varchar(8) NOT NULL,
+  `semester` varchar(6) NOT NULL,
+  `year` decimal(4,0) NOT NULL,
+  `building` varchar(15) NOT NULL,
+  `room_number` varchar(7) NOT NULL,
+  `time_slot_id` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
