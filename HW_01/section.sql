67764b2ee2568eb4918ac2588a2e2c50c2b1ab8d
1009 <a329224119@gmail.com>
2025-03-11 14:31:31 +0800

    資料補齊

--- HW_01/section.sql
+++ HW_01/section.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.6.6
 -- https://www.phpmyadmin.net/
 --
--- 主機： 127.0.0.1
--- 產生時間： 2025-03-10 09:48:42
--- 伺服器版本： 10.4.32-MariaDB
--- PHP 版本： 8.2.12
+-- 主機: localhost
+-- 產生時間： 2025-03-11 06:30:42
+-- 伺服器版本: 5.7.17-log
+-- PHP 版本： 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -35,8 +34,28 @@ CREATE TABLE `section` (
   `building` varchar(15) NOT NULL,
   `room_number` varchar(7) NOT NULL,
   `time_slot_id` varchar(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 資料表的匯出資料 `section`
+--
+
+INSERT INTO `section` (`course_id`, `sec_id`, `semester`, `year`, `building`, `room_number`, `time_slot_id`) VALUES
+('BIO-101', '1', 'Summer', '2009', 'Painter', '514', 'B'),
+('BIO-301', '1', 'Summer', '2010', 'Painter', '514', 'A'),
+('CS-101', '1', 'Fall', '2009', 'Packard', '101', 'H'),
+('CS-101', '1', 'Spring', '2010', 'Packard', '101', 'F'),
+('CS-190', '1', 'Spring', '2009', 'Taylor', '3128', 'E'),
+('CS-190', '2', 'Spring', '2009', 'Taylor', '3128', 'A'),
+('CS-315', '1', 'Spring', '2010', 'Watson', '120', 'D'),
+('CS-319', '1', 'Spring', '2010', 'Watson', '100', 'B'),
+('CS-319', '2', 'Spring', '2010', 'Taylor', '3128', 'C'),
+('CS-347', '1', 'Fall', '2009', 'Taylor', '3128', 'A'),
+('EE-181', '1', 'Spring', '2009', 'Taylor', '3128', 'C'),
+('FIN-201', '1', 'Spring', '2010', 'Packard', '101', 'B'),
+('HIS-351', '1', 'Spring', '2010', 'Painter', '514', 'C'),
+('MU-199', '1', 'Spring', '2010', 'Packard', '101', 'D'),
+('PHY-101', '1', 'Fall', '2009', 'Watson', '100', 'A');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
