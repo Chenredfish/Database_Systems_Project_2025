7c325f1e16f9843366b930dd13b52631d21ba43b
1009 <a329224119@gmail.com>
2025-05-19 22:06:40 +0800

    初始化方式變更

--- final_project/scr/object/equipment.gd
+++ final_project/scr/object/equipment.gd
@@ -5,3 +5,10 @@ var name: String
 var attack_defence: int
 var magic_defence: int
 var level: int
+
+func _init(data: Dictionary) -> void:
+	id = data.get("id", 0)
+	name = data.get("name", "")
+	attack_defence = data.get("attack_defence", 0)
+	magic_defence = data.get("magic_defence", 0)
+	level = data.get("level", 0)
