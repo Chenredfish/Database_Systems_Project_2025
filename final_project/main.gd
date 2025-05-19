4e06caa10eebfdd41755e5e1e4a010adb30f69d5
redfish <chenredfish940309@gmsil.com>
2025-05-19 21:35:48 +0800

    隨機抓一個等級0的actor當作玩家

--- final_project/main.gd
+++ final_project/main.gd
@@ -5,7 +5,20 @@ extends Node2D
 #UI
 @onready var ui_layer = $UILayer
 
+@onready var db = SQLite.new()
+
 func _ready():
+	
+	randomize()
+	
+	db.path = "res://data/game"
+	db.open_db()
+	
+	var level_0_actor_id:Array = db.select_rows("actor", "level = 0", ["id"])
+	var number_level_0_actor:int = level_0_actor_id.size()
+	var rand_actor_id = randi()%number_level_0_actor
+	var rand_actor_data = db.select_rows("actor", "id = " + str(rand_actor_id), ["*"])
+	#print(rand_actor_data[0]["name"])
 	
 	ui_layer.show()
 	game_state_machine.launch()
