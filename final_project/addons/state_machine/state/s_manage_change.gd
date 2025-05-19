2cf093942f9f26f3d9265c6ab08f160bfbda0816
redfish <chenredfish940309@gmsil.com>
2025-05-19 20:49:53 +0800

    新增進入到修改非ring的狀態，並且導入to_manage_something當作狀態機管理的變數

--- final_project/addons/state_machine/state/s_manage_change.gd
+++ final_project/addons/state_machine/state/s_manage_change.gd
@@ -1,11 +1,18 @@
 extends BaseState
 class_name s_Manage_Change
 
+var aim:String = ""
+
 func enter(_msg:Dictionary = {}):
 	print("into state of s_Manage_Change")
+	
+	aim = state_machine.get_value('to_manage_something')
+	
+	print("show " + aim)
 
 func update(delta):
 	pass
 	
 func exit():
-	pass
+	aim = ""
+	state_machine.set_value('to_manage_something', aim)
--- final_project/addons/state_machine/state/s_manage_main.gd
+++ final_project/addons/state_machine/state/s_manage_main.gd
@@ -3,15 +3,21 @@ class_name s_Manage_Main
 
 var to_playing:bool = false
 var to_manage_ring:bool = false
+var to_manage_something:String = ""
 
 func enter(_msg:Dictionary = {}):
 	print("into state of s_Manage_Main")
+	
+	#這是一個跨階段的變數，以防萬一先初始化
+	to_manage_something = ""
 	
 	agent.ui_layer.show_ui_manage_main()
 	if !agent.ui_layer.exit_manage_main.is_connected(exit_manage_main):
 		agent.ui_layer.exit_manage_main.connect(exit_manage_main)
 	if !agent.ui_layer.to_manage_ring.is_connected(change_state_manage_ring):
 		agent.ui_layer.to_manage_ring.connect(change_state_manage_ring)
+	if !agent.ui_layer.to_manage_something.is_connected(change_state_manage_change):
+		agent.ui_layer.to_manage_something.connect(change_state_manage_change)
 
 func update(delta):
 
@@ -26,6 +32,12 @@ func update(delta):
 		
 	if to_manage_ring:
 		transform_to(StateEnum.GAME_STATE_TYPE.MANAGE_CHANGE_RING)
+		
+	if state_machine.has_value('to_manage_something'):
+		to_manage_something = state_machine.get_value('to_manage_something')
+		
+	if to_manage_something:
+		transform_to(StateEnum.GAME_STATE_TYPE.MANAGE_CHANGE)
 	
 func exit():
 	to_playing = false
@@ -42,3 +54,6 @@ func exit_manage_main():
 	
 func change_state_manage_ring():
 	state_machine.set_value('to_manage_ring', true)
+
+func change_state_manage_change(aim:String):
+	state_machine.set_value('to_manage_something', aim)
