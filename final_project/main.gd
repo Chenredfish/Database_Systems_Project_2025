fc3f2387af698e4c2a9cb6f884fb0ac1230a0c48
redfish <chenredfish940309@gmail.com>
2025-05-20 14:52:22 +0800

    修改玩家建立的地方，原本在main，改成在s_playing新增

--- final_project/main.gd
+++ final_project/main.gd
@@ -5,24 +5,12 @@ extends Node2D
 #UI
 @onready var ui_layer = $UILayer
 
-@onready var db = SQLite.new()
 
 
 
 func _ready():
 	
 	randomize()
-	
-	db.path = "res://data/game"
-	db.open_db()
-	
-	var level_0_actor_id:Array = db.select_rows("actor", "level = 0", ["id"])
-	var number_level_0_actor:int = level_0_actor_id.size()
-	var rand_actor_id = randi() % number_level_0_actor + 1 #id不會是0，但是%出來會出現0
-	var rand_actor_data = db.select_rows("actor", "id = " + str(rand_actor_id), ["*"])
-	#print(rand_actor_data[0])
-	
+
 	ui_layer.show()
 	game_state_machine.launch()
-	
-	game_state_machine.set_value('player', Actor.new(rand_actor_data[0]))
