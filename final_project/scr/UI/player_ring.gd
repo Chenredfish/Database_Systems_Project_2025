098299222d769c1967ad69a3e6af7c5a67470768
redfish <chenredfish940309@gmsil.com>
2025-05-22 17:40:36 +0800

    新增show_ring的程式

---
+++ final_project/scr/UI/player_ring.gd
@@ -0,0 +1,40 @@
+extends GridContainer
+
+@onready var ring_1: PanelContainer = $ring1
+@onready var ring_page = 0
+
+var search_ring_data:Array[Ring] = []
+
+func _ready() -> void:
+	ring_1.hide()
+
+func input_show_ring_data(actor:Actor):
+	search_ring_data = actor.get("rings")
+	_page_update()
+	
+func delete_show_ring_data():
+	for child in self.get_children():
+		if child.name.begins_with("new_ring"):
+			child.queue_free()
+
+func _page_update():	#刷新頁面
+	for child in self.get_children():
+		if child.name.begins_with("new_ring"):
+			child.queue_free()
+	var count = ring_page * 6
+	while count < ring_page * 6 + 6 and count < search_ring_data.size():
+		_add_ring(count)
+		count += 1
+
+func _add_ring(ring_n):
+	var new_ring = ring_1.duplicate()
+	new_ring.name = "new_ring" + str(ring_n)
+	add_child(new_ring)
+	new_ring.show()
+	new_ring.get_node("VBC/ring_name").text = str(search_ring_data[ring_n].get("name"))
+	new_ring.get_node("VBC/ring_level").text = "光環階級：" + str(search_ring_data[ring_n].get("level"))
+	new_ring.get_node("VBC/ring_health").text = "生命值：" + str(search_ring_data[ring_n].get("health"))
+	new_ring.get_node("VBC/ring_attack_power").text = "物理攻擊：" + str(search_ring_data[ring_n].get("attack_power"))
+	new_ring.get_node("VBC/ring_magic_power").text = "魔法攻擊：" + str(search_ring_data[ring_n].get("magic_power"))
+	new_ring.get_node("VBC/ring_attack_defence").text = "物理防禦：" + str(search_ring_data[ring_n].get("attack_defence"))
+	new_ring.get_node("VBC/ring_magic_defence").text = "魔法防禦：" + str(search_ring_data[ring_n].get("magic_defence"))
---
+++ final_project/scr/UI/ui_show_ring.gd
@@ -0,0 +1,38 @@
+extends Control
+
+signal exit_show_ring
+
+@onready var ring: GridContainer = $MarginContainer/HBoxContainer/ring_list/VBoxContainer/MarginContainer/ring
+
+@onready var health = $MarginContainer/HBoxContainer/character_status/PanelContainer/MarginContainer/GridContainer/health
+@onready var level = $MarginContainer/HBoxContainer/character_status/PanelContainer/MarginContainer/GridContainer/level
+@onready var type = $MarginContainer/HBoxContainer/character_status/PanelContainer/MarginContainer/GridContainer/type
+@onready var attack_power = $MarginContainer/HBoxContainer/character_status/PanelContainer/MarginContainer/GridContainer/attack_power
+@onready var magic_power = $MarginContainer/HBoxContainer/character_status/PanelContainer/MarginContainer/GridContainer/magic_power
+@onready var attack_defence = $MarginContainer/HBoxContainer/character_status/PanelContainer/MarginContainer/GridContainer/attack_defence
+@onready var magic_defence = $MarginContainer/HBoxContainer/character_status/PanelContainer/MarginContainer/GridContainer/magic_defence
+
+
+func _on_exit_button_pressed():
+	exit_show_ring.emit()
+
+func input_show_ring_data(actor:Actor):
+	ring.input_show_ring_data(actor)
+	show_actor_data(actor)
+
+func delete_show_ring_data():
+	ring.delete_show_ring_data()
+
+func show_actor_data(actor:Actor):
+	health.text = str(actor.get("health"))
+	level.text = str(actor.get("level"))
+	
+	#取名錯誤，type已經改名為element
+	type.text = str(actor.get("element"))
+	#這裡有取名錯誤
+	attack_power.text = str(actor.get("attack_point"))
+	magic_power.text = str(actor.get("magic_point"))
+	
+	attack_defence.text = str(actor.get("attack_defence"))
+	magic_defence.text = str(actor.get("magic_defence"))
+	
