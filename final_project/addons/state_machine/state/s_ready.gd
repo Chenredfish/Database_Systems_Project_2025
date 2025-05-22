6aa36bdb142a0b841705c4dd0f9cc519eeaeee18
redfish <chenredfish940309@gmsil.com>
2025-05-22 21:45:06 +0800

    把敵人節給刪除

--- final_project/addons/state_machine/state/s_ready.gd
+++ final_project/addons/state_machine/state/s_ready.gd
@@ -9,17 +9,18 @@ var available_equipment = null
 
 func enter(_msg: Dictionary = {}):
 	print("into state of s_Ready")
+	
+	db.path = "res://data/game"
+	db.open_db()
 
 	var level = state_machine.get_value("level")
 	var player = state_machine.get_value("player")
 	
 	update_level()
 	
-	#free_old_enemy()
+	free_old_enemy()
 	create_level_enemy(level)
 
-	db.path = "res://data/game"
-	db.open_db()
 
 	available_skills = fetch_random_skills(level)
 	available_rings = fetch_random_rings(level)
@@ -59,11 +60,12 @@ func fetch_random_equipment(level: int) -> Dictionary:
 	return results[0] if results.size() > 0 else null
 
 func free_old_enemy():
-	if agent.is_ancestor_of(state_machine.get_value('enemy')):
-		agent.enemy.queue_free()
+	for child in agent.get_children():
+		if child.name == "enemy":
+			child.queue_free()
 		
 func create_level_enemy(level:int):
-	var enemy_data:Array = db.select_rows("actor", "level = " + str(level), ["*"])
+	var enemy_data = db.select_rows("actor", "level = " + str(level), ["*"])
 	if enemy_data.size() > 0:
 		var rand_enemy_id:int = randi() % enemy_data.size()
 		state_machine.set_value('enemy', Actor.new(enemy_data[rand_enemy_id]))
