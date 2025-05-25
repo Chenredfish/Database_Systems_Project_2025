0d3a5e93d8842caee8ed63cef37a636ea996ab68
redfish <chenredfish940309@gmsil.com>
2025-05-26 07:31:42 +0800

    敵人照片

--- final_project/scr/UI/ready_equipment_list.gd
+++ final_project/scr/UI/ready_equipment_list.gd
@@ -6,8 +6,14 @@ extends MarginContainer
 
 @export var max_amount_in_one_page:int = 5
 
+var UI_normal_theme = load("res://assets/UI_Theme.tres")
+var UI_chosen_theme = load("res://assets/UI_chosen_theme.tres")
 var search_equipment_data:Array[Equipment] = []
 var all_equipment_data:Array[Equipment] = []
+<<<<<<< HEAD
+=======
+var equipment_chosen
+>>>>>>> e5afa4e2638a0a7ea518931f931ac0dc2cca78aa
 
 func _ready() -> void:
 	equipment_1.hide()
@@ -32,6 +38,7 @@ func _page_update():	#刷新頁面
 	while count < equipment_page * max_amount_in_one_page + max_amount_in_one_page and count < search_equipment_data.size():
 		_add_equipment(count)
 		count += 1
+<<<<<<< HEAD
 		
 func _search_equipment(keyword:String):
 	var count = 0
@@ -51,10 +58,14 @@ func _search_equipment(keyword:String):
 	await get_tree().process_frame
 	#print(search_ring_data)
 	_page_update()
+=======
+	print("refreshed")
+>>>>>>> e5afa4e2638a0a7ea518931f931ac0dc2cca78aa
 
 func _add_equipment(equipment_n):
 	var new_equipment = equipment_1.duplicate()
 	new_equipment.name = "new_equipment" + str(equipment_n)
+	var equipment_choose_btn = new_equipment.get_node("HBoxContainer/equipment_choose/equipment_choose_btn")
 	v_box_container.add_child(new_equipment)
 	new_equipment.show()
 	new_equipment.get_node("HBoxContainer/equipment_id/Label").text = str(search_equipment_data[equipment_n].get("id"))
@@ -62,3 +73,12 @@ func _add_equipment(equipment_n):
 	new_equipment.get_node("HBoxContainer/equipment_level/Label").text = str(search_equipment_data[equipment_n].get("level"))
 	new_equipment.get_node("HBoxContainer/equipment_attack_defence/Label").text = str(search_equipment_data[equipment_n].get("attack_defence"))
 	new_equipment.get_node("HBoxContainer/equipment_magic_defence/Label").text = str(search_equipment_data[equipment_n].get("magic_defence"))
+	equipment_choose_btn.pressed.connect(Callable(self, "_equipment_choose").bind(equipment_n, new_equipment))
+
+func _equipment_choose(equipment_num, equipment):
+	for child in v_box_container.get_children():
+		if child.name != "new_equipment" + str(equipment_num):
+			child.theme = UI_normal_theme
+		else:
+			child.theme = UI_chosen_theme
+			equipment_chosen = equipment
