cb965101e66f486eec35758994e67e96831d923e
redfish <chenredfish940309@gmsil.com>
2025-05-25 16:01:15 +0800

    一部分的ready顯示裝備的介面

---
+++ final_project/scr/UI/ready_equipment_list.gd
@@ -0,0 +1,32 @@
+extends MarginContainer
+
+@onready var equipment_1 = $VBoxContainer/equipment1
+@onready var v_box_container = $VBoxContainer
+@onready var equipment_page = 0
+
+var search_equipment_data:Array[Equipment] = []
+var all_ring_data:Array[Equipment] = []
+
+func _ready() -> void:
+	equipment_1.hide()
+
+func input_show_equipment_data(actor:Actor):
+	search_equipment_data = actor.get("equipment_list")
+	all_ring_data = search_equipment_data
+	_page_update()
+	
+func _page_update():	#刷新頁面
+	for child in self.get_children():
+		if child.name.begins_with("new_ring"):
+			child.queue_free()
+	var count = equipment_page * 6
+	while count < equipment_page * 6 + 6 and count < search_equipment_data.size():
+		_add_ring(count)
+		count += 1
+
+func _add_ring(equipment_n):
+	var new_equipment = equipment_1.duplicate()
+	new_equipment.name = "new_ring" + str(equipment_n)
+	v_box_container.add_child(new_equipment)
+	new_equipment.show()
+	#new_equipment.get_node("VBC/ring_name").text = str(search_equipment_data[equipment_n].get("name"))
