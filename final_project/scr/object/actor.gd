590e5a4556094b66f0455c00eaa721d91927f6e0
1009 <a329224119@gmail.com>
2025-06-03 11:20:08 +0800

    data

--- final_project/scr/object/actor.gd
+++ final_project/scr/object/actor.gd
@@ -88,10 +88,20 @@ func _init(data: Dictionary) -> void:
 		skill_change(level_skill[rand_skill_id]["id"])
 
 func open_data():
-	db.path = "res://data/game.db"
-	if not db.open_db():
-		push_error("無法開啟資料庫")
-		return
+	var db_dir = "user://data"
+	var db_path = db_dir + "/game.db"
+	# 確保 user://data 資料夾存在
+	if not DirAccess.dir_exists_absolute(db_dir):
+		DirAccess.make_dir_recursive_absolute(db_dir)
+	# 如果 user://data/game.db 不存在，從 res://data/game.db 複製過去
+	if not FileAccess.file_exists(db_path):
+		var src = FileAccess.open("res://data/game.db", FileAccess.READ)
+		var dst = FileAccess.open(db_path, FileAccess.WRITE)
+		dst.store_buffer(src.get_buffer(src.get_length()))
+		src.close()
+		dst.close()
+	db.path = db_path
+	db.open_db()
 
 
 func add_equipment_to_list(equipment_id: int):#將裝備加入擁有清單中
